25ce4a6c520f46eddf543c9617db57e72bb3df0a
iftakhersifat <hossensifat615@gmail.com>
2025-05-04 20:46:48 +0600

    added navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavLink } from 'react-router';
+
+const Navbar = () => {
+    return (
+        <div className='container mx-auto mt-4'>
+            <div className='flex justify-between items-center'>
+            <div>
+                <img className='w-38' src="/assets/logo.png" alt="" />
+            </div>
+            <div className='space-x-6'>
+                <NavLink to="/apps">Apps</NavLink>
+                <NavLink to="/profile">My Profile</NavLink>
+            </div>
+            <div>
+                <button className='btn bg-gradient-to-b from-blue-400 to-blue-700 text-white px-6 rounded-lg'>Login</button>
+            </div>
+        </div>
+        </div>
+    );
+};
+
+export default Navbar;
