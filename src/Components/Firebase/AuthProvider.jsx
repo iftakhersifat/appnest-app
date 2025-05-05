b9a91ee82f6802da4f948e534468c66720bf10fb
iftakhersifat <hossensifat615@gmail.com>
2025-05-05 10:21:50 +0600

    added setUser

--- src/Components/Firebase/AuthProvider.jsx
+++ src/Components/Firebase/AuthProvider.jsx
@@ -1,14 +1,26 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import React, { createContext, useState } from 'react';
+import { createUserWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
+import React, { createContext, useEffect, useState } from 'react';
 import { auth } from './Firebase';
 export const AUthContext = createContext();
 
 const AuthProvider = ({children}) => {
     const [user, setUser] =useState(null);
+    console.log(user)
 
+    // new user
     const createUser=(email, password)=>{
         return createUserWithEmailAndPassword(auth, email, password);
     }
+
+    // hold login info
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth, (currentUser)=>{
+            setUser(currentUser);
+        });
+        return ()=>{
+            unsubscribe();
+        }
+    },[])
 
 
     const authData ={
