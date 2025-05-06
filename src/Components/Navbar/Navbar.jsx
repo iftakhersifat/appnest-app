6a83fdf8b5824b4bb5227c61529ac7d7d734194f
iftakhersifat <hossensifat615@gmail.com>
2025-05-06 19:38:05 +0600

    added updated parts

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -24,17 +24,17 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
                       </div>
                 
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                    <NavLink to="/" className={({ isActive }) => isActive ? "text-blue-500 underline text-[18px]" : "text-[18px]"}>Home</NavLink>
-                    <NavLink to="/profile" className={({ isActive }) => isActive ? "text-blue-500 underline text-[18px]" : "text-[18px]"}>My Profile</NavLink>
-                    <NavLink to="/about" className={({ isActive }) => isActive ? "text-blue-500 underline text-[18px]" : "text-[18px]"}>About Us</NavLink>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content border-1 border-blue-200 bg-gradient-to-b from-blue-400 to-blue-700 text-white bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                    <NavLink to="/" className={({ isActive }) => isActive ? "text-green-500 underline text-[18px]" : "text-[18px]"}>Home</NavLink>
+                    <NavLink to="/profile" className={({ isActive }) => isActive ? "text-green-500 underline text-[18px]" : "text-[18px]"}>My Profile</NavLink>
+                    <NavLink to="/about" className={({ isActive }) => isActive ? "text-green-500 underline text-[18px]" : "text-[18px]"}>About Us</NavLink>
                     </ul>
                 
                     </div>
                 
                     {/* image sections */}
                     <div>
-                        <img className='w-42 -ml-4' src="/assets/logo.png" alt="" />
+                        <img className='w-42 -ml-4' src="/assets/logo.png" alt="logo" />
                     </div>
                 
                   </div>
